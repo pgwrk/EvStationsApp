04884e11a41a87aeafa5bb3983230cb96e9fabfe
Pavel <pgczwork@gmail.com>
2022-11-09 17:39:58 +0300

    Added mocks for future StationsViewModel's unit tests

---
+++ app/src/test/java/com/pgsoft/evstationsapp/StationsViewModelTests.kt
@@ -0,0 +1,31 @@
+package com.pgsoft.evstationsapp
+
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+
+class StationsViewModelTests {
+
+    @Before
+    fun setup() {}
+
+    @Test
+    fun uiState_whenInitialized_thenLoading() {
+        assertTrue(false)
+    }
+
+    @Test
+    fun uiState_whenStationsLoadingError_thenShowError() {
+        assertTrue(false)
+    }
+
+    @Test
+    fun uiState_whenStationsLoaded_thenShowContent() {
+        assertTrue(false)
+    }
+
+    @Test
+    fun uiState_whenStationsLoaded_thenStationSortedByDistance() {
+        assertTrue(false)
+    }
+}
